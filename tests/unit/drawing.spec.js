ba9ed9606c072b2b32da9deaeb36f6a40746e8d9
René Muhl <rmuhl@exa-online.de>
2019-10-11 14:40:39 +0200

    Add unit test

---
+++ tests/unit/drawing.spec.js
@@ -0,0 +1,18 @@
+import { mount } from "@vue/test-utils"
+import drawing from "@/components/drawing"
+
+describe("Drawing component", () => {
+  it("shows a drawing if at least a date is given", () => {
+    const value = {
+      date: "2000-01-01"
+    }
+    const wrapper = mount(drawing, {
+      propsData: {
+        value
+      }
+    })
+
+    expect(wrapper.html()).toContain(value.date)
+
+  })
+})
