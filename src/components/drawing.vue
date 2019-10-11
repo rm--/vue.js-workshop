340ae19948b8964db2e2fa08611c029ee62338bd
René Muhl <rmuhl@exa-online.de>
2019-10-11 10:23:00 +0200

    Change data model

--- src/components/drawing.vue
+++ src/components/drawing.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="drawing">{{value.Datum}}</div>
+  <div id="drawing">
+    <div>{{value.date}}</div>
+  </div>
 </template>
 
 <script>
@@ -7,15 +9,9 @@ export default {
   name: "drawing",
   data: () => ({
     value: {
-      Datum: "2018-01-05T00:00:00",
-      ez1: 10,
-      ez2: 7,
-      Id: 1,
-      z1: 40,
-      z2: 2,
-      z3: 38,
-      z4: 45,
-      z5: 7
+      date: "2018-01-05",
+      extranumbers: [10,7],
+      numbers: [40,2,38,45,7]
     }
   })
 };
