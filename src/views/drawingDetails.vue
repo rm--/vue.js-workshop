86c63fe6c5fb3fcbb5b7e9cd12b459e2697e4264
René Muhl <rmuhl@exa-online.de>
2019-10-11 15:04:37 +0200

    Add drawingDetails

---
+++ src/views/drawingDetails.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container id="drawingDetails">
+        <v-card>
+          <v-card-text>
+            <drawing :value="drawing"></drawing>
+          </v-card-text>
+        </v-card>
+  </v-container>
+</template>
+
+<script>
+import drawing from "@/components/drawing";
+
+export default {
+  name: "drawingDetails",
+  components: { drawing },
+  data() {
+    return {
+      drawing: {date: "Hey"}
+    }
+  }
+};
+</script>
