938ecd16c76d2f68b3d4fe1575cd0ab27b64237d
René Muhl <rmuhl@exa-online.de>
2019-10-11 10:16:43 +0200

    Return drawing date

--- src/components/drawing.vue
+++ src/components/drawing.vue
@@ -1,9 +1,11 @@
 <template>
-  <div id="drawing">hallo LE</div>
+  <div id="drawing">{{value.Datum}}</div>
 </template>
 
 <script>
 export default {
-  name: "drawing"
+  name: "drawing",
+  data: function() {
+    return {value: {"Datum":"2018-01-05T00:00:00","ez1":10,"ez2":7,"Id":1,"z1":40,"z2":2,"z3":38,"z4":45,"z5":7}}}
 };
 </script>
