fbe9785331d80537ab9c1109f963b39ec396a16c
René Muhl <rmuhl@exa-online.de>
2019-10-11 10:18:22 +0200

    Change to anonym function

--- src/components/drawing.vue
+++ src/components/drawing.vue
@@ -5,7 +5,7 @@
 <script>
 export default {
   name: "drawing",
-  data: function() {
-    return {value: {"Datum":"2018-01-05T00:00:00","ez1":10,"ez2":7,"Id":1,"z1":40,"z2":2,"z3":38,"z4":45,"z5":7}}}
+  data: () => ({
+   value: {"Datum":"2018-01-05T00:00:00","ez1":10,"ez2":7,"Id":1,"z1":40,"z2":2,"z3":38,"z4":45,"z5":7}})
 };
 </script>
