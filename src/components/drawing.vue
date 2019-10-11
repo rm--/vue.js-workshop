871fa1fb0db9e6ea1fb9c80796c87263b213c137
René Muhl <rmuhl@exa-online.de>
2019-10-11 10:30:22 +0200

    Show numbers and extra numbers

--- src/components/drawing.vue
+++ src/components/drawing.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="drawing">
     <div>{{value.date}}</div>
+    <div>
+      <span v-for="(item,index) in value.numbers" :key="index">{{item}} </span>
+    </div>
+    <div>
+      <span v-for="(item,index) in value.extranumbers" :key="index">{{item}} </span>
+    </div>
   </div>
 </template>
 
@@ -10,8 +16,8 @@ export default {
   data: () => ({
     value: {
       date: "2018-01-05",
-      extranumbers: [10,7],
-      numbers: [40,2,38,45,7]
+      extranumbers: [10, 7],
+      numbers: [40, 2, 38, 45, 7]
     }
   })
 };
