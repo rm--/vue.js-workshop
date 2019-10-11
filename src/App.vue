4e37077e486af7859cc1bc4e378c45e611c2d32f
René Muhl <rmuhl@exa-online.de>
2019-10-11 10:46:11 +0200

    Format app.vue

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,14 @@ export default {
   components: {
     drawing
   },
-  data () {
+  data() {
     return {
       drawingData: {
-      date: "2018-01-05",
-      extranumbers: [10, 7],
-      numbers: [40, 2, 38, 45, 7]
-    }
-  }
+        date: "2018-01-05",
+        extranumbers: [10, 7],
+        numbers: [40, 2, 38, 45, 7]
+      }
+    };
   }
 };
 </script>
